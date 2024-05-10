4d41653e29c5185ab802977dbaaa99b6f749f48f
susan <susangithaiga26@gmail.com>
2024-05-10 08:06:28 +0300

    feat: Build the UI 💄

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,22 @@
 <script setup>
-import { useBoardStore } from '../stores/boardStore';
-    const boardStore = useBoardStore()
-
+import { useBoardStore } from "../stores/boardStore";
+const boardStore = useBoardStore();
 </script>
 <template>
-  <main>
-    <h1>Build a trello clone home page</h1>
-    <pre> {{ boardStore.board }}</pre>
-  </main>
+  <div class="board-wrapper">
+    <main class="board">
+      <!-- <h1>{{ boardStore.board.name }}</h1> -->
+      <UContainer  v-for="column in boardStore.board.columns" :key="column.name" class="column">
+        <h2 class="mb-4">{{ column.name }}</h2>
+        <ul>
+          <li v-for="task in column.tasks" :key="task.id">
+          <UCard class="mb-4">
+            <strong> {{ task.name }}</strong>
+            <p>{{ task.description }}</p>
+          </UCard>
+          </li>
+        </ul>
+      </UContainer >
+    </main>
+  </div>
 </template>
